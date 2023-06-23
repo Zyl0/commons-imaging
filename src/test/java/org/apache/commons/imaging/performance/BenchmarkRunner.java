1030aece631d7964c582cba02ad91833735f43df
Zyl0 <cyprien.tourondecourteix2@gmail.com>
2023-06-23 16:42:32 +0200

    Disabled benchmarks

--- src/test/java/org/apache/commons/imaging/performance/BenchmarkRunner.java
+++ src/test/java/org/apache/commons/imaging/performance/BenchmarkRunner.java
@@ -17,7 +17,7 @@
 package org.apache.commons.imaging.performance;
 
 public class BenchmarkRunner {
-    public static void main(String[] args) throws Exception {
-        org.openjdk.jmh.Main.main(args);
-    }
+//    public static void main(String[] args) throws Exception {
+//        org.openjdk.jmh.Main.main(args);
+//    }
 }
